5eff2c9d52275c9b02869f4ae4ff6d5ea0c889b5
psharratt <psharratt1@gmail.com>
2023-05-07 23:02:57 +0200

    Update recursive_bucket.py

--- recursive_bucket.py
+++ recursive_bucket.py
@@ -50,8 +50,8 @@ def recursive_bucket_sort(array, bucket_size=5):
 """
 Testing recursive_bucket_sort() on:
     - an array of mixed data types
-    - array of floats
-    - array of integers
+    - an array of floats
+    - an array of integers
 """
 array_mixed = [-4, 42, 32, 23, 25, 52, -1.3, 47, 751, 100,0.1,10,0.2]
 array_floats=[0.1,0.11,0.13,0.893,0.2]
