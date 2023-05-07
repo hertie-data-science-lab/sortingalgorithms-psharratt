90da38358c9bc1f693a2eeb5a08e9155516657f4
psharratt <psharratt1@gmail.com>
2023-05-08 00:42:27 +0200

    edited test - adding random array generator

--- test.py
+++ test.py
@@ -5,26 +5,44 @@ Submitted on 7th May 2023
 
 SUBMISSION ASSIGNMENT 10 
 
-@authors: Fabian Metz | Amin Oueslati | Oskar Krafft | Paul Sharratt
+Authors: Fabian Metz | Amin Oueslati | Oskar Krafft | Paul Sharratt
 
-"""
-from recursive_bucket import recursive_bucket_sort
-
-"""
 Testing recursive_bucket_sort() on:
-    - an array of mixed data types
-    - an array of floats
-    - an array of integers
+    - random arrays of mixed data types, including negative numbers
     - an empty array
+
 """
-array_mixed = [-4, 42, 32, 23, 25, 52, -1.3, 47, 751, 100,0.1,10,0.2]
-array_floats = [0.1,0.11,0.13,0.893,0.2]
-array_ints =[42, 32, 23, 25, 52, 47]
-array_empty = []
 
-# Use the recursive_bucket_sort function to sort the mixed, float, ints arrays 
-print(recursive_bucket_sort(array_mixed))
-print(recursive_bucket_sort(array_floats)) 
-print(recursive_bucket_sort(array_ints))
-print(recursive_bucket_sort(array_empty))
+from recursive_bucket import recursive_bucket_sort
+import random
 
+def generate_random_array(size, min_value, max_value):
+    """
+    Generates a random array with a specified size, containing integers and floats.
+    
+    Args:
+        size (int): The size of the array.
+        min_value (int or float): The minimum value for generated numbers.
+        max_value (int or float): The maximum value for generated numbers.
+
+    Returns:
+        list: a randomly generated array.
+
+    """
+    array = []
+    for _ in range(size):
+        if random.choice([True, False]):
+            # Generate a random integer
+            array.append(random.randint(min_value, max_value))
+        else:
+            # Generate a random float with two decimal places
+            array.append(round(random.uniform(min_value, max_value), 2))
+    return array
+
+random_array_1 = generate_random_array(10, 1, 20)
+random_array_2 = generate_random_array(5, -2, 2)
+empty_array = []
+
+print("Random Test Array 1:", random_array_1, "\nSorted Test Array 1:", recursive_bucket_sort(random_array_1), sep='')
+print("Random Test Array 2:", random_array_2, "\nSorted Test Array 2:", recursive_bucket_sort(random_array_2), sep='')
+print("Empty Array:", empty_array, "\nSorted Empty Array:", recursive_bucket_sort(empty_array), sep='')
